db7592bb6e41e20764792ffafa4a6b467da9c855
Federico Holgado <fed@mailchimp.com>
2020-10-11 20:49:39 -0400

    Hello world with Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">FHL.GD</header>
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
