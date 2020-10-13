bfbd1c566589434c15f7358492a22852d7e70f1f
Federico Holgado <fed@mailchimp.com>
2020-10-13 08:24:18 -0400

    Link netlify site

---
+++ functions/airtable.js
@@ -0,0 +1,40 @@
+// lambda/airtable.js
+const Airtable = require("airtable");
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
+  apiKey: process.env.AIRTABLE_API_KEY,
+});
+const base = Airtable.base("appZIHDZoIDR2armb");
+
+exports.handler = function (event, context, callback) {
+  const allRecords = [];
+  base("Table 1")
+    .select({
+      maxRecords: 100,
+      view: "grid",
+    })
+    .eachPage(
+      function page(records, fetchNextPage) {
+        records.forEach(function (record) {
+          allRecords.push(record);
+        });
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          callback(err);
+        } else {
+          const body = JSON.stringify({ records: allRecords });
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              "content-type": "application/json",
+              "cache-control": "Cache-Control: max-age=300, public",
+            },
+          };
+          callback(null, response);
+        }
+      }
+    );
+};
